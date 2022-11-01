c92f53da3fa8bb54adde31379bbb07d1bbf9501f
Mikołaj J <mikigraf@wp.pl>
2022-11-01 19:30:04 +0100

    Initial commit

---
+++ src/main/java/com/example/EnglishShopMainApp/EnglishShopMainAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnglishShopMainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishShopMainAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishShopMainAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EnglishShopMainApp/EnglishShopMainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnglishShopMainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishShopMainAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
